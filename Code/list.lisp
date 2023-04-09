f471f684d36c461e320c24dc715035ed54cd7212
Robert Strandh <robert.strandh@gmail.com>
2023-04-09 16:58:49 +0200

    Define compiler macro for LIST*.

--- Code/list.lisp
+++ Code/list.lisp
@@ -10,16 +10,35 @@
 (declaim (inline list*))
 
 (defun list* (object &rest objects)
-  (if (null objects)
-      object
-      (loop with head = (cons object nil)
-            for tail = head then (cdr tail)
-            for rest = objects then first
-            for first = (cdr objects) then (cdr first)
-            until (null first)
-            do (setf (cdr tail)
-                     (cons (car rest) nil))
-            finally (setf (cdr tail) (car rest))
-                    (return head))))
+  (cons object (reverse objects)))
+  ;; (if (null objects)
+  ;;     object
+  ;;     (loop with head = (cons object nil)
+  ;;           for tail = head then (cdr tail)
+  ;;           for rest = objects then first
+  ;;           for first = (cdr objects) then (cdr first)
+  ;;           until (null first)
+  ;;           do (setf (cdr tail)
+  ;;                    (cons (car rest) nil))
+  ;;           finally (setf (cdr tail) (car rest))
+  ;;                   (return head))))
+
 
 (declaim (notinline list*))
+
+(define-compiler-macro list*
+    (&whole form object-form &rest object-forms)
+  (cond ((null object-forms)
+         object-form)
+        ((> (length object-forms) 10)
+         form)
+        (t
+         (labels ((nest (forms)
+                    (if (null (cdr forms))
+                        (car forms)
+                        `(cons ,(car forms)
+                               ,(nest (cdr forms))))))
+           `(cons ,object-form
+                  ,(nest object-forms))))))
+    
+  
