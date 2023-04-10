3ddf9ef47a56c52b9f1c4f167f5f300daa0789e3
Robert Strandh <robert.strandh@gmail.com>
2023-04-10 08:16:49 +0200

    Define function MEMBER-IF-NOT.

--- Code/member.lisp
+++ Code/member.lisp
@@ -63,3 +63,14 @@
          (funcall predicate (car rest)))
         (special-function
          (funcall predicate (funcall key (car rest)))))))
+
+(defun member-if-not (predicate list &key key)
+  (macrolet ((special-function (test)
+               `(with-proper-list-rests (rest list)
+                  (unless ,test
+                    (return rest)))))
+    (if (or (null key) (eq key #'identity) (eq key 'identity))
+        (special-function
+         (funcall predicate (car rest)))
+        (special-function
+         (funcall predicate (funcall key (car rest)))))))
