1cbe80dcac9704770498088e221677ff3fd1035f
Robert Strandh <robert.strandh@gmail.com>
2023-04-11 06:26:02 +0200

    Detect circular lists in LAST.

--- Code/last.lisp
+++ Code/last.lisp
@@ -5,14 +5,30 @@
 (defun last (list &optional (n 1))
   (check-type list cl:list)
   (check-type n (integer 0))
-  (let ((head list)
-        (tail list))
-    (loop repeat n
-          until (atom tail)
-          do (pop tail))
-    (loop until (atom tail)
-          do (pop tail)
-             (pop head))
-    head))
+  ;; We make an explicit test for the empty list, because the form (EQ
+  ;; SLOW TAIL) would otherwise return true, and we would incorrectly
+  ;; report the list as being circular.
+  (if (null list)
+      nil
+      ;; The CONS cells starting at HEAD will be returned.  TAIL is
+      ;; used to detect the end of the list.
+      (let ((tail list))
+        (loop repeat n
+              until (atom tail)
+              do (pop tail))
+        (loop with head = list
+              with slow = list
+              until (atom tail)
+              do (pop tail)
+                 (pop head)
+              until (atom tail)
+              do (pop tail)
+                 (pop head)
+                 (pop slow)
+              until (eq slow tail)
+              finally (if (eq slow tail)
+                          (error 'circular-list
+                                 "Circular list ~s" list)
+                          (return head))))))
 
 (declaim (notinline last))
