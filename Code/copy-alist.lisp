4963817f2e3522bcb04c558f8ab7e1ba40041b0b
Robert Strandh <robert.strandh@gmail.com>
2023-04-12 18:11:12 +0200

    Account for the possibility that the element is an atom.

--- Code/copy-alist.lisp
+++ Code/copy-alist.lisp
@@ -5,7 +5,9 @@
 (defun copy-alist (alist)
   (let ((reversed-result '()))
     (with-alist-elements (element alist)
-      (push (cons (car element) (cdr element))
+      (push (if (consp element)
+                (cons (car element) (cdr element))
+                element)
             reversed-result))
     (nreverse reversed-result)))
 
