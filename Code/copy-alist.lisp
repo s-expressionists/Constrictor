ce9a58b49e93b859e520ae8cb04ed5a457c00896
Robert Strandh <robert.strandh@gmail.com>
2023-04-12 17:17:59 +0200

    Use macro WITH-ALIST-ELEMENTS.

--- Code/copy-alist.lisp
+++ Code/copy-alist.lisp
@@ -3,14 +3,11 @@
 (declaim (inline copy-alist))
 
 (defun copy-alist (alist)
-  (loop for rest on alist
-        for element = (car rest)
-        if (atom element)
-          do (error 'must-be-alist "Must be alist ~s" alist)
-        else
-          collect (cons (car element) (cdr element))
-        finally (unless (listp rest)
-                  (error 'must-be-alist "Must be alist ~s" alist))))
+  (let ((reversed-result '()))
+    (with-alist-elements (element alist)
+      (push (cons (car element) (cdr element))
+            reversed-result))
+    (nreverse reversed-result)))
 
 (declaim (notinline copy-alist))
 
