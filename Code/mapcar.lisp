af14622abd06be0dced46c5c4185b1d26e7a7e19
Robert Strandh <robert.strandh@gmail.com>
2023-04-13 09:28:48 +0200

    Handle special case of 2 lists, and add comment.

--- Code/mapcar.lisp
+++ Code/mapcar.lisp
@@ -1,20 +1,43 @@
 (cl:in-package #:constrictor)
 
+;;; It is probably not worthwhile to define a compiler macro for
+;;; MAPCAR.  One could perhaps avoid the allocation of a &REST list
+;;; that way, but by definition MAPCAR allocates memory anyway, so it
+;;; might not be worth the effort.  However, it is probably worthwhile
+;;; to handle the cases of one and two lists specially.  This is
+;;; because the general case allocates a new list in each iteration.
+
 (declaim (inline mapcar))
 
 (defun mapcar (function &rest lists)
-  (let ((local-lists lists))
-    (cond ((null local-lists)
-           (error 'at-least-one-list-must-be-supplied))
-          ((null (rest local-lists))
-           (let ((list (first local-lists)))
-             (loop for rest on list
-                   until (atom rest)
-                   collect (funcall function (first rest))
-                   finally (unless (listp rest)
-                             (error 'list-must-be-proper
-                                    :datum list)))))
-          (t
+  (cond ((null lists)
+         (error 'at-least-one-list-must-be-supplied))
+        ((null (rest lists))
+         ;; We have exactly one list.  This is of couse a very
+         ;; common special case.
+         (let ((list (first lists)))
+           (loop for rest on list
+                 until (atom rest)
+                 collect (funcall function (first rest))
+                 finally (unless (listp rest)
+                           (error 'list-must-be-proper
+                                  :datum list)))))
+        ((null (rest (rest lists)))
+         ;; We have exactly two lists.  Another common special case.
+         (let ((list1 (first lists))
+               (list2 (second lists)))
+           (loop for rest1 on list1
+                 for rest2 on list2
+                 until (or (atom rest1) (atom rest2))
+                 collect (funcall function (first rest1) (first rest2))
+                 finally (unless (listp rest1)
+                           (error 'list-must-be-proper
+                                  :datum list1))
+                         (unless (listp rest2)
+                           (error 'list-must-be-proper
+                                  :datum list2)))))
+        (t
+         (let ((local-lists lists))
            (locally (declare (notinline mapcar))
              (loop until (some #'atom local-lists)
                    collect (apply function (mapcar #'first local-lists))
