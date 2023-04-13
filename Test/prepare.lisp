004bd9618c48025c68dba15892d06c8aa7677d1c
Robert Strandh <robert.strandh@gmail.com>
2023-04-13 14:05:25 +0200

    Shadow the MAP family symbols.

--- Test/prepare.lisp
+++ Test/prepare.lisp
@@ -3,7 +3,10 @@
 (shadowing-import
  '(constrictor:last constrictor:butlast
    constrictor:subst constrictor:subst-if constrictor:subst-if-not
-   constrictor:assoc constrictor:assoc-if constrictor:assoc-if-not)
+   constrictor:assoc constrictor:assoc-if constrictor:assoc-if-not
+   constrictor:mapcar constrictor:mapc constrictor:maplist
+   constrictor:mapl constrictor:mapcan constrictor:mapcon)
+ 
  '#:cl-test)
 
 (let ((*default-pathname-defaults*
