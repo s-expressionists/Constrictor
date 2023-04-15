6f4297a9272b0e67cc0405b2532f5961ac4c96cb
Robert Strandh <robert.strandh@gmail.com>
2023-04-15 07:35:00 +0200

    Define ASSOC-IF using new macros and function ASSOC-IF-CORE.

--- Code/assoc-if.lisp
+++ Code/assoc-if.lisp
@@ -1,16 +1,19 @@
 (cl:in-package #:constrictor)
 
+(declaim (inline assoc-if-core))
+
+(defun assoc-if-core (predicate alist key)
+  (with-key (key)
+    (with-alist-elements (element alist)
+      (when (funcall predicate (apply-key (car element)))
+        (return-from assoc-if-core element)))))
+
+(declaim (notinline assoc-if-core))
+
 (declaim (inline assoc-if))
 
 (defun assoc-if (predicate alist &key key)
-  (macrolet ((special-function (test)
-               `(with-alist-elements (element alist)
-                  (when ,test
-                    (return-from assoc-if element)))))
-    (if (or (null key) (eq key #'identity) (eq key 'identity))
-        (special-function
-         (funcall predicate (car element)))
-        (special-function
-         (funcall predicate (funcall key (car element)))))))
+  (with-canonical-key (key)
+    (assoc-core predicate alist key)))
 
 (declaim (notinline assoc-if))
