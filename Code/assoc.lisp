d05c8a73fd3c4c6c93543ec6d9729437dd3b43cd
Robert Strandh <robert.strandh@gmail.com>
2023-04-17 06:15:53 +0200

    Define compiler macro for ASSOC.

--- Code/assoc.lisp
+++ Code/assoc.lisp
@@ -24,3 +24,50 @@
     (assoc-core item alist key test test-not)))
 
 (declaim (notinline assoc))
+
+(define-compiler-macro assoc
+    (&whole form item-form alist-form &rest arguments)
+  (let ((triples (process-keyword-arguments
+                  arguments '(:key :test :test-not))))
+    (when (eq triples :unacceptable)
+      (return-from assoc form))
+    (when (and (find-triple :test triples)
+               (find-triple :test-not triples))
+      (return-from assoc form))
+    (multiple-value-bind (let-bindings ignorable-declaration)
+        (compute-let-bindings-and-declarations triples)
+      `(let ,let-bindings
+         ,ignorable-declaration
+         (assoc-core
+          ,item-form
+          ,alist-form
+          ,(let ((triple (find-triple :key triples)))
+             (if (or (null triple)
+                     (member (second triple) '(nil 'identity #'identity)
+                             :test #'equal))
+                 #'identity
+                 (third triple)))
+          ,(let ((triple (find-triple :test triples)))
+             (cond ((find-triple :test-not triples)
+                    'nil)
+                   ((or (null triple)
+                        (member (second triple) '(#'eql 'eql)
+                                :test #'equal))
+                    #'eql)
+                   ((member (second triple) '(#'eq 'eq)
+                            :test #'equal)
+                    #'eq)
+                   (t
+                    (third triple))))
+          ,(let ((triple (find-triple :test-not triples)))
+             (cond ((or (find-triple :test triples)
+                        (null triple))
+                    'nil)
+                   ((member (second triple) '(#'eq 'eq)
+                            :test #'equal)
+                    #'eq)
+                   ((member (second triple) '(#'eql 'eql)
+                            :test #'equal)
+                    #'eql)
+                   (t
+                    (third triple)))))))))
