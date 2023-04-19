7534c97639f91baf5b574c38b5e99de686b56f21
Robert Strandh <robert.strandh@gmail.com>
2023-04-19 15:10:47 +0200

    Define embryonic function CHECK-CALL-SITE.

--- Code/compiler-macro-support.lisp
+++ Code/compiler-macro-support.lisp
@@ -23,7 +23,17 @@
 ;;;    of three symbols.  The first symbol is the keyword that should
 ;;;    be recognized in the call.  The second symbol is the parameter
 ;;;    itself.  the third symbol is a "supplied-p" parameter that
-;;;    works in the same wasy as for optional parameters
+;;;    works in the same wasy as for optional parameters.  If there
+;;;    are no keyword parameters, but the original lambda list has the
+;;;    lambda-list keword &KEY in it, then this element is T.
 ;;;
 ;;;  * The symbol &ALLOW-OTHER-KEYS or NIL if other keys are not
 ;;;    allowed.
+
+(defun check-call-site (arguments lambda-list)
+  (destructuring-bind (required optional rest key allow-other-keys)
+      lambda-list
+    ;; There must be at least as many arguments as there are required
+    ;; parameters.
+    (unless (>= (length arguments) required)
+      (return-from check-call-site nil))))
