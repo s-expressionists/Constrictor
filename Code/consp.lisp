1a0910bb213a7b17e68ccf762509859f3079325c
Robert Strandh <robert.strandh@gmail.com>
2023-04-23 16:37:43 +0200

    Provide documentation for LISTP.

--- Code/consp.lisp
+++ Code/consp.lisp
@@ -13,7 +13,7 @@
   (:method (object) t)
   (:method ((object cons)) nil))
 
-(setf (documentation 'ATOM 'function)
+(setf (documentation 'atom 'function)
       (format nil "ATOM OBJECT~@
                    Return T if the object given as an argument is an atom,~@
                    i.e., an object other than a CONS cell.  Return NIL if~@
@@ -23,3 +23,8 @@
   (:method (object) nil)
   (:method ((object cons)) t)
   (:method ((object null)) t))
+
+(setf (documentation 'listp 'function)
+      (format nil "LISTP OBJECT~@
+                   Return T if the object given as an argument is a list, i.e.,~@
+                   either a CONS cell or the symbol NIL.  Otherwise, return NIL."))
