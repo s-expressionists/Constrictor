322e6ef24f61bfd19a8ee6fecc5a21a5834f41da
Robert Strandh <robert.strandh@gmail.com>
2023-04-23 16:31:52 +0200

    Provide documentation for CONSP.

--- Code/consp.lisp
+++ Code/consp.lisp
@@ -4,6 +4,11 @@
   (:method (object) nil)
   (:method ((object cons)) t))
 
+(setf (documentation 'consp 'function)
+      (format nil "CONSP OBJECT~@
+                   Return T if the object given as an argument is a CONS cell.~@
+                   Otherwise, return NIL."))
+
 (defgeneric atom (object)
   (:method (object) t)
   (:method ((object cons)) nil))
