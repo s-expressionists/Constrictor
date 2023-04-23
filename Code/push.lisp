db5a06518ddfa9c9687d662b0b24f6325e932c27
Robert Strandh <robert.strandh@gmail.com>
2023-04-23 09:34:28 +0200

    Provide documentation for PUSH.

--- Code/push.lisp
+++ Code/push.lisp
@@ -8,3 +8,13 @@
               ,@(mapcar #'list vars vals)
               (,(car store-vars) (cons ,item-var ,reader-form)))
          ,writer-form))))
+
+(setf (documentation 'push 'function)
+      (format nil "PUSH ITEM PLACE~@
+                   Modify PLACE so that it contains a CONS cell with~@
+                   ITEM in the CAR slot and the old value of PLACE in~@
+                   the CDR slot.  Return the new contents of PLACE.~@
+                   ~@
+                   (PUSH ITEM PLACE) has the same effect and the same~@
+                   return value as (SETF PLACE (CONS ITEM PLACE)) except~@
+                   that PUSH evaluates sub-forms of PLACE only once."))
