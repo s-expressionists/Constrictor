68411375b02ae51431274db52999216113dbd4be
Robert Strandh <robert.strandh@gmail.com>
2023-04-23 16:34:19 +0200

    Provide documentation for ATOM.

--- Code/consp.lisp
+++ Code/consp.lisp
@@ -13,6 +13,12 @@
   (:method (object) t)
   (:method ((object cons)) nil))
 
+(setf (documentation 'ATOM 'function)
+      (format nil "ATOM OBJECT~@
+                   Return T if the object given as an argument is an atom,~@
+                   i.e., an object other than a CONS cell.  Return NIL if~@
+                   the object given is a CONS cell."))
+
 (defgeneric listp (object)
   (:method (object) nil)
   (:method ((object cons)) t)
