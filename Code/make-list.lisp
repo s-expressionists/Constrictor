d79e3e79112ff3b5638851be22187a8d79c9b203
Robert Strandh <robert.strandh@gmail.com>
2023-04-30 10:44:07 +0200

    Use LOOP keyword COLLECT to construct the list.

--- Code/make-list.lisp
+++ Code/make-list.lisp
@@ -2,11 +2,7 @@
 
 (declaim (inline make-list))
 
-;;; We could have used the COLLECT LOOP keyword, but it is then likely
-;;; that there is an additional test. 
 (defun make-list (length &key (initial-element nil))
   (check-type length (integer 0))
-  (loop with result = '()
-        repeat length
-        do (push initial-element result)
-        finally (return result)))
+  (loop repeat length
+        collect initial-element))
