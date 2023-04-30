de3a59f005eeecc4e77434e4abbe91f12d12499a
Robert Strandh <robert.strandh@gmail.com>
2023-04-30 11:39:04 +0200

    Provide documentation for MAPLIST.

--- Code/maplist.lisp
+++ Code/maplist.lisp
@@ -52,3 +52,18 @@
                                :datum (elt local-lists position)))))))))
 
 (declaim (notinline maplist))
+
+(setf (documentation 'maplist 'function)
+      (format nil
+              "maplist function &rest lists~@
+               Apply FUNCTION to first to each list in LISTS~@
+               then to the REST of each list in LISTS, and so on.~@
+               Collect the results of these applications into~@
+               a list which is returned as the value of MAPLIST.~@
+               Iteration stops when the shortest LIST is exhausted.~@
+               Each LIST must be a proper list.  If one of the ~@
+               shortest lists is a dotted list, then an error~@
+               of type TYPE-ERROR is signaled.  If every list in~@
+               LISTS is a circular list, then this function will~@
+               not terminate.  If LISTS is empty then an error~@
+               is signaled."))
