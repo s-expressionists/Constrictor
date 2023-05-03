7c677f2964dc81cb145c1700189f91c702a12e46
Robert Strandh <robert.strandh@gmail.com>
2023-05-03 15:11:27 +0200

    Surround definition by INLINE/NOTINLINE proclamations.

--- Code/revappend.lisp
+++ Code/revappend.lisp
@@ -1,7 +1,11 @@
 (cl:in-package #:constrictor)
 
+(declaim (inline revappend))
+
 (defun revappend (list tail)
   (let ((result tail))
     (with-proper-list-elements (element list)
       (push element result))
     result))
+
+(declaim (notinline revappend))
