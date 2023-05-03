c6937713cc076647e827ae107de9a7ac12e4d39b
Robert Strandh <robert.strandh@gmail.com>
2023-05-03 15:10:29 +0200

    Use macro WITH-PROPER-LIST-ELEMENTS in code for REVAPPEND.

--- Code/revappend.lisp
+++ Code/revappend.lisp
@@ -1,13 +1,7 @@
 (cl:in-package #:constrictor)
 
 (defun revappend (list tail)
-  (let ((remaining list)
-        (result tail))
-    (loop until (atom remaining)
-          do (push (pop remaining) result)
-          finally (if (null remaining)
-                      (return result)
-                      (error 'list-must-be-proper
-                             :datum remaining
-                             :expected-type 'list
-                             :offending-list list)))))
+  (let ((result tail))
+    (with-proper-list-elements (element list)
+      (push element result))
+    result))
