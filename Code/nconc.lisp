d67136f1df168f6c934f8efdbfd5ae93a276e703
Robert Strandh <robert.strandh@gmail.com>
2023-05-04 07:16:44 +0200

    Provide documentation for NCONC.

--- Code/nconc.lisp
+++ Code/nconc.lisp
@@ -11,3 +11,16 @@
             finally (return (first lists)))))
 
 (declaim (notinline nconc))
+
+(setf (documentation 'nconc 'function)
+      (format nil
+              "Syntax: nconc &rest lists~@
+               Concatenate the objects in LISTS by destructively~@
+               modifying the last CONS cell of every object except~@
+               the last one so that it refers to the next object in~@
+               order.  The last object in LISTS may be any object.~@
+               Every object other than the last must be a proper list~@
+               or a dotted list.  If an object other than the last is~@
+               not a list, an error of type TYPE-ERROR is signaled.~@
+               If an object other than the last is a circular list~@
+               an error is signaled."))
