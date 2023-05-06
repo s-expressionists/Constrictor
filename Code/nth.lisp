876299e37a927efdadc5c0948b0c55d2df6e2c46
Robert Strandh <robert.strandh@gmail.com>
2023-05-06 17:29:56 +0200

    Provide documentation for NTH.

--- Code/nth.lisp
+++ Code/nth.lisp
@@ -18,3 +18,13 @@
                                 :offending-list list)))))
 
 (declaim (notinline nth))
+
+(setf (documentation 'nth 'function)
+      (format nil
+              "Syntax: nth n list~@
+               Return the Nth element of LIST, where N=0~@
+               means the first element of LIST.  LIST may be~@
+               a dotted list or a circular list, but if LIST~@
+               has at most N CONS cells, then an error is~@
+               signaled.  If N is not a non-negative integer,~@
+               then an error is signaled."))
