c4ade017207d87fd38b7f59c3fd2e2b29448ac3f
Robert Strandh <robert.strandh@gmail.com>
2023-05-10 16:07:56 +0200

    Fix incorrect implementation of PUSHNEW.

--- Code/pushnew.lisp
+++ Code/pushnew.lisp
@@ -3,10 +3,19 @@
 (defmacro pushnew
     (item place
      &environment environment
-     &rest args)
-  (multiple-value-bind (vars vals store-vars writer-form reader-form)
-      (get-setf-expansion place environment)
-    `(let (,@(mapcar #'list vars vals))
-       (let ((,(car store-vars)
-               (adjoin ,item ,reader-form ,@args)))
-         ,writer-form))))
+     &key
+       (key nil key-supplied-p)
+       (test nil test-supplied-p)
+       (test-not nil test-not-supplied-p))
+  (if (and test-supplied-p test-not-supplied-p)
+      (progn (warn 'warn-both-test-and-test-not-given)
+             `(error 'both-test-and-test-not-given))
+      (multiple-value-bind (vars vals store-vars writer-form reader-form)
+          (get-setf-expansion place environment)
+        `(let (,@(mapcar #'list vars vals))
+           (let ((,(car store-vars)
+                   (adjoin-core ,item ,reader-form
+                                ,key ,key-supplied-p
+                                ,test ,test-supplied-p
+                                ,test-not ,test-not-supplied-p)))
+             ,writer-form)))))
