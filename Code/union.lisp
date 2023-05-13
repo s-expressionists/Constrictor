085d11d38e69bdd5a989c41ad55304aed1d4e315
Robert Strandh <robert.strandh@gmail.com>
2023-05-13 07:59:47 +0200

    Bug fix.

--- Code/union.lisp
+++ Code/union.lisp
@@ -6,13 +6,13 @@
     (list-1 list-2 key key-supplied-p test test-supplied-p test-not test-not-supplied-p)
   (with-key (key key-supplied-p)
     (with-test (test test-supplied-p test-not test-not-supplied-p)
-      (loop with result = '()
+      (loop with result = list-2
             for element-1 in list-1
-            do (push element-1 result)
-               (loop for element-2 in list-2
-                     unless (apply-test (apply-key element-1)
-                                        (apply-key element-2))
-                       do (push element-2 result))
+            do (loop for element-2 in list-2
+                     when (apply-test (apply-key element-1)
+                                      (apply-key element-2))
+                       return nil
+                       finally (push element-1 result))
             finally (return result)))))
 
 (declaim (notinline union-core))
