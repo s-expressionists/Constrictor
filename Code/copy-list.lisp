681a2e7cf5dc3067ad75dea48c8f287b0aa8dcd4
Robert Strandh <robert.strandh@gmail.com>
2023-05-14 10:26:32 +0200

    Provide documentation for COPY-LIST.

--- Code/copy-list.lisp
+++ Code/copy-list.lisp
@@ -18,3 +18,22 @@
                        (return head)))))
 
 (declaim (notinline copy-list))
+
+(setf (documentation 'copy-list 'function)
+      (format nil
+              "Syntax copy-list list~@
+               ~@
+               LIST must be a proper list or a dotted list.~@
+               If a circular list is given as the value of LIST~@
+               then this function will not terminate.  If LIST is~@
+               an atom, then an error of type TYPE-ERROR will be~@
+               signaled.  If LIST is a circular list, this function~@
+               will not terminate.~@
+               ~@
+               The list structure of LIST is copied, so that the~@
+               return value of this function contains a copy of~@
+               each of the top-level CONS cells of LIST.  Each copy~@
+               contains the same CAR as the original cons cell, and~@
+               the last CONS cell of the value returned form this~@
+               function contains the same CDR as the last CONS cell~@
+               of LIST."))
