118dc3115bf3fc3aff551a89f16f80e614137f32
Robert Strandh <robert.strandh@gmail.com>
2023-05-15 11:27:29 +0200

    Provide documentation for MAKE-LIST.

--- Code/make-list.lisp
+++ Code/make-list.lisp
@@ -6,3 +6,12 @@
   (check-type length (integer 0))
   (loop repeat length
         collect initial-element))
+
+(setf (documentation 'make-list 'function)
+      (format nil
+              "Syntax: make-list length &optional iniital-element~@
+               ~@
+               Create and return a proper list with LENGTH CONS~@
+               cells in it.  Each element of the list is the~@
+               object INITIAL-ELEMENT.  If INITIAL-ELEMENT is not~@
+               given, it defaults to NIL."))
