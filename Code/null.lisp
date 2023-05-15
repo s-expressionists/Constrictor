cf248e76a125ae3fef040c50ab8c9c30600767fd
Robert Strandh <robert.strandh@gmail.com>
2023-05-15 11:27:15 +0200

    Provide documentation for NULL.

--- Code/null.lisp
+++ Code/null.lisp
@@ -2,3 +2,10 @@
 
 (defun null (object)
   (eq object nil))
+
+(setf (documentation 'null 'function)
+      (format nil
+              "Syntax: null object~@
+               ~@
+               Return true if an only if OBJECT is the object NIL.~@
+               Rerutn NIL otherwise."))
