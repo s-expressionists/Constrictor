4f0ef4810dd9c3e38f51608aec6d1aaf8d18547a
Robert Strandh <robert.strandh@gmail.com>
2023-05-17 17:24:29 +0200

    Add documentation for ACONS.

--- Code/acons.lisp
+++ Code/acons.lisp
@@ -2,3 +2,16 @@
 
 (defun acons (key datum alist)
   (cons (cons key datum) alist))
+
+(setf (documentation 'acons 'function)
+      (format nil
+              "Syntax: acons key datum alist~@
+               ~@
+               This function adds a new entry to the front of~@
+               the association list ALIST.  The entry is itself~@
+               a CONS cell cell where KEY is the CAR and DATUM~@
+               is the CDR.~@
+               ~@
+               No checks are made as to the structure of ALIST~@
+               so it can be any object.  Therefore, this function~@
+               is equivalent to (CONS (CONS KEY DATUM) ALIST)."))
