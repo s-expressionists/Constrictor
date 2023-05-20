ce31d400253d5420aece6f7668bf1db7e4641d1c
Robert Strandh <robert.strandh@gmail.com>
2023-05-20 06:13:06 +0200

    Provide documentation for ENDP.

--- Code/endp.lisp
+++ Code/endp.lisp
@@ -7,3 +7,12 @@
   (null list))
 
 (declaim (notinline endp))
+
+(setf (documentation 'endp 'function)
+      (format nil
+              "Syntax: endp list~@
+               ~@
+               This function returns true if LIST is NIL, and it returns~@
+               NIL if LIST is a CONS cell.  If LIST is not a list, i.e.,~@
+               neither NIL or a CONS cell, an error of type TYPE-ERROR~@
+               is signaled."))
