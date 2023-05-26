204b1c5be6e29c0800b900b47f78a5c82babd288
Robert Strandh <robert.strandh@gmail.com>
2023-05-26 05:55:52 +0200

    Provide documentation for NTHCDR.

--- Code/nthcdr.lisp
+++ Code/nthcdr.lisp
@@ -18,3 +18,19 @@
                                 :offending-list list)))))
 
 (declaim (notinline nthcdr))
+
+(setf (documentation 'nthcdr 'function)
+      (format nil
+              "Syntax: nthcdr n list~@
+               ~@
+               This function returns the tail of LIST resulting from~@
+               applying the function CDR, N times to LIST.~@
+               ~@
+               N is a non-negative integer.  If N is not a non-negative~@
+               integer, then an error of type TYPE-ERROR is signaled.~@
+               LIST is a list that can be proper, dotted, or circular.~@
+               If LIST is not a list, an error of type TYPE-ERROR is~@
+               signaled.
+               ~@
+               If LIST has fewer than N CONS cells, then an error is~@
+               signaled."))
