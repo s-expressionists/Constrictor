d4b19263d83bd47b7951ed7e523c37d3d71002ec
Robert Strandh <robert.strandh@gmail.com>
2023-05-26 07:32:00 +0200

    Provide documentation for ASSOC.

--- Code/assoc.lisp
+++ Code/assoc.lisp
@@ -46,3 +46,27 @@
        key key-supplied-p
        test test-supplied-p
        test-not test-not-supplied-p))))
+
+(setf (documentation 'assoc 'function)
+      (format nil
+              "Syntax assoc item alist &key key test test-not~@
+               ~@
+               This function returns the first CONS cell in ALIST~@
+               that satisfies the test, i.e., the CAR of which is~@
+               compares with ITEM to yield a true value, in terms~@
+               of KEY, TEST, and TEST-NOT.  If no CONS cell satisfies~@
+               the test, then this function returns NIL.~@
+               ~@
+               KEY must be NIL or a designator for a function of~@
+               one argument.  If KEY is NIL, then it is as if the~@
+               function named IDENTITY had been supplied instead.~@
+               Either of TEST or TEST-NOT must be a designator for~@
+               a function of two arguments.  If neither TEST nor~@
+               TEST-NOT is given, then it is as if the function named~@
+               EQL had been supplied as the value of TEST.  If both~@
+               TEST and TEST-NOT are supplied, then an error is signaled.~@
+               ~@
+               If ALIST contains an element that is not permitted in~@
+               an association list, and that element occurs before the~@
+               first element that satisfies the test, then an error~@
+               of type TYPE-ERROR is signaled."))
