2efde6d2a5425009cfb34e82d510af58422497d8
Robert Strandh <robert.strandh@gmail.com>
2023-05-26 08:01:53 +0200

    Provide documentation for RASSOC-IF.

--- Code/rassoc-if.lisp
+++ Code/rassoc-if.lisp
@@ -16,3 +16,22 @@
   (rassoc-core predicate alist key key-supplied-p))
 
 (declaim (notinline rassoc-if))
+
+(setf (documentation 'rassoc-if 'function)
+      (format nil
+              "Syntax rassoc-if predicate alist &key key~@
+               ~@
+               This function returns the first CONS cell in ALIST~@
+               the CDR of which satisfies the test, i.e., returns~@
+               true when the KEY and the PREDICATE are applied to it.~@
+               If there is no CONS cell in ALIST with a CDR that~@
+               satisfies the test, then this function returns NIL.~@
+               ~@
+               KEY must be NIL or a designator for a function of~@
+               one argument.  If KEY is NIL, then it is as if the~@
+               function named IDENTITY had been supplied instead.~@
+               ~@
+               If ALIST contains an element that is not permitted in~@
+               an association list, and that element occurs before the~@
+               first element that satisfies the test, then an error~@
+               of type TYPE-ERROR is signaled."))
