eb616409d46bca6e5ad3d7ff65c698c1b79b2dcc
Robert Strandh <robert.strandh@gmail.com>
2023-05-28 10:18:29 +0200

    Define compiler macro for APPEND.

--- Code/append.lisp
+++ Code/append.lisp
@@ -1,5 +1,7 @@
 (cl:in-package #:constrictor)
 
+(declaim (inline append))
+
 (defun append (&rest lists)
   (if (null lists)
       '()
@@ -25,3 +27,34 @@
                               (error 'list-must-be-proper
                                      :offending-list object))))))
         result)))
+
+(declaim (notinline append))
+
+(define-compiler-macro append (&whole form &rest list-forms)
+  (case (length list-forms)
+    (0
+     '())
+    (1
+     (first list-forms))
+    (2 
+     (let ((first-form-variable (gensym))
+           (second-form-variable (gensym))
+           (copy-variable (gensym))
+           (last-variable (gensym)))
+       `(let ((,first-form-variable ,(first list-forms))
+              (,second-form-variable ,(second list-forms)))
+          (cond ((null ,first-form-variable)
+                 ,second-form-variable)
+                ((atom ,first-form-variable)
+                 (error 'type-error
+                        :datum ,first-form-variable
+                        :expected-type 'cl:list))
+                (t
+                 (let* ((,copy-variable (copy-list ,first-form-variable))
+                        (,last-variable (last ,copy-variable)))
+                   (if (null (cdr ,last-variable))
+                       (progn (rplacd ,last-variable ,second-form-variable)
+                              ,copy-variable)
+                       (error 'list-must-be-proper
+                              :offending-list ,first-form-variable))))))))
+    (otherwise form)))
