16f6b04567bd2021bba0ff14b6bb0c3bc8c4f0fe
Robert Strandh <robert.strandh@gmail.com>
2023-05-28 10:59:44 +0200

    Define function COPY-LIST-AND-LAST like COPY-LIST and LAST combined.

--- Code/utilities.lisp
+++ Code/utilities.lisp
@@ -23,3 +23,42 @@
   (unless (proper-list-p list)
     (error 'list-must-be-proper
            :offending-list list)))
+
+(defun copy-list-and-last (list)
+  (cond ((null list)
+         '())
+        ((atom list)
+         (error 'list-expected :datum list))
+        (t
+         (loop with head = (cons (car list) nil)
+               for tail = head then (cdr tail)
+               for rest = (cdr list) then (cdr rest)
+               until (atom rest)
+               do (setf (cdr tail)
+                        (cons (car rest) nil))
+               finally (setf (cdr tail) rest)
+                       (return (values head tail))))))
+
+(setf (documentation 'copy-list-and-last 'function)
+      (format nil
+              "Syntax: copy-list-and-ast list~@
+               ~@
+               LIST must be a proper list or a dotted list.~@
+               If a circular list is given as the value of LIST~@
+               then this function will not terminate.  If LIST is~@
+               an atom, then an error of type TYPE-ERROR will be~@
+               signaled.  If LIST is a circular list, this function~@
+               will not terminate.~@
+               ~@
+               The list structure of LIST is copied, so that the~@
+               return value of this function contains a copy of~@
+               each of the top-level CONS cells of LIST.  Each copy~@
+               contains the same CAR as the original cons cell, and~@
+               the last CONS cell of the value returned form this~@
+               function contains the same CDR as the last CONS cell~@
+               of LIST.~@
+               ~@
+               If LIST is a CONS cell, then this function returns a~@
+               second value which is the last CONS cell of the first~@
+               return value."))
+
