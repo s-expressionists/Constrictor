5399ef439771c2168bd454b6c14286f3b2a2118a
Robert Strandh <robert.strandh@gmail.com>
2023-05-28 11:00:17 +0200

    Define COPY-LIST in terms of COPY-LIST-AND-LAST.

--- Code/copy-list.lisp
+++ Code/copy-list.lisp
@@ -3,19 +3,7 @@
 (declaim (inline copy-list))
 
 (defun copy-list (list)
-  (cond ((null list)
-         '())
-        ((atom list)
-         (error 'list-expected :datum list))
-        (t
-         (loop with head = (cons (car list) nil)
-               for tail = head then (cdr tail)
-               for rest = (cdr list) then (cdr rest)
-               until (atom rest)
-               do (setf (cdr tail)
-                        (cons (car rest) nil))
-               finally (setf (cdr tail) rest)
-                       (return head)))))
+  (values (copy-list-and-last list)))
 
 (declaim (notinline copy-list))
 
