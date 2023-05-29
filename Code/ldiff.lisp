162fbfeed565f1d4e791213a72796814a1fb9454
Robert Strandh <robert.strandh@gmail.com>
2023-05-29 07:33:42 +0200

    Provide documentation for LDIFF.

--- Code/ldiff.lisp
+++ Code/ldiff.lisp
@@ -10,3 +10,19 @@
         when (eq object tail)
           return (copy-front list object)
         finally (return (copy-list list))))
+
+(setf (documentation 'ldiff 'function)
+      (format nil
+              "Syntax: ldiff list object~@
+               ~@
+               If OBJECT is EQ to some top-level CONS cell of LIST,~@
+               then this function returns a list consisting of copies~@
+               of the CONS cells that precede OBJECT in LIST, with OBJECT~@
+               replaced by NIL.  If OBJECT is not EQ to some tail of LIST,~@
+               then a copy of LIST is returned.~@
+               ~@
+               LIST must be a list that might be a dotted list.~@
+               If LIST is not a list, then an error of type TYPE-ERROR~@
+               is signaled.  If LIST is a circular list, and no top-level~@
+               CONS cell is EQ to OBJECt, then this function does not~@
+               terminate."))
