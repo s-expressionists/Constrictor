f759adc64eb223c5068ba74c94a83e0691aa5219
Robert Strandh <robert.strandh@gmail.com>
2023-05-29 10:36:39 +0200

    Provide documentation for COPY-TREE.

--- Code/copy-tree.lisp
+++ Code/copy-tree.lisp
@@ -11,3 +11,14 @@
     (copy-tree-local tree)))
 
 (declaim (notinline copy-tree))
+
+(setf (documentation 'copy-tree 'function)
+      (format nil
+              "Syntax: copy-tree tree~@
+               ~@
+               This function returns a copy of TREE.  If TREE is~@
+               an atom, then it is returned.  If not, this function~@
+               returnes a new CONS cell which contains the result of~@
+               recursively calling COPY-TREE on the CAR of TREE in its~@
+               CAR slot, and the result of recursively calling COPY-TREE~@
+               on the CDR of TREE in its CDR slot."))
