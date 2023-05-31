505508bfe0014f5b500e7baf08c1b3484582752e
Robert Strandh <robert.strandh@gmail.com>
2023-05-31 05:42:45 +0200

    Provide documentation for ADJOIN.

--- Code/adjoin.lisp
+++ Code/adjoin.lisp
@@ -46,3 +46,21 @@
        key key-supplied-p
        test test-supplied-p
        test-not test-not-supplied-p))))
+
+(setf (documentation 'adjoin 'function)
+      (format nil
+              "Syntax adjoin item list &key key test test-not~@
+               ~@
+               This function tests whether ITEM is an element~@
+               of LIST, as defined by KEY and either TEST or~@
+               TEST-NOT.  If ITEM is an element of LIST, then~@
+               LIST is returned.  Otherwise, a new list is returned~@
+               consisting of a new CONS cell with ITEM in its CAR~@
+               slot and with LIST in its CDR slot~@
+               ~%~a~%~%~a~%~%~a~%~%~a~%~@
+               If LIST is a circular list, and no element of LIST~@
+               satisfies the test, then this function will not terminate."
+              *key*
+              *key-applied-to-elements-of-list*
+              *test-and-test-not*
+              *list-should-be-proper*))
