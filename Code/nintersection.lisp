8b5c5ddc2c73a86f0b0fd4dd0e8ebd66225e79ee
Robert Strandh <robert.strandh@gmail.com>
2023-05-31 05:26:51 +0200

    Provide compiler macro for NINTERSECTION.

--- Code/nintersection.lisp
+++ Code/nintersection.lisp
@@ -44,6 +44,26 @@
                       test test-supplied-p
                       test-not test-not-supplied-p))
 
+(define-compiler-macro nintersection (&whole form &rest arguments)
+  (let ((lambda-list
+          '((list-1 list-2) ; required
+            ()           ; optional
+            nil          ; rest
+            ((:key key key-supplied-p)
+             (:test test test-supplied-p)
+             (:test-not test-not test-not-supplied-p))
+            nil)))
+    (unless (check-call-site arguments lambda-list)
+      (return-from nintersection form))
+    (compute-compiler-macro-body
+     arguments
+     (butlast lambda-list)
+     '(nintersection-core
+       list-1 list-2
+       key key-supplied-p
+       test test-supplied-p
+       test-not test-not-supplied-p))))
+
 (setf (documentation 'nintersection 'function)
       (format nil
               "Syntax: nintersection list-1 list-2 &key key test test-not~@
