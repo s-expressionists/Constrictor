f99004b9f669caf29e601a0d749e73000af8607a
Robert Strandh <robert.strandh@gmail.com>
2023-06-01 07:48:19 +0200

    Provide compiler macros for SET-EXCLUSIVE-OR and NSET-EXCLUSIVE-OR.

--- Code/set-exclusive-or.lisp
+++ Code/set-exclusive-or.lisp
@@ -37,3 +37,23 @@
                          key key-supplied-p
                          test test-supplied-p
                          test-not test-not-supplied-p))
+
+(define-compiler-macro set-exclusive-or (&whole form &rest arguments)
+  (let ((lambda-list
+          '((list-1 list-2) ; required
+            ()           ; optional
+            nil          ; rest
+            ((:key key key-supplied-p)
+             (:test test test-supplied-p)
+             (:test-not test-not test-not-supplied-p))
+            nil)))
+    (unless (check-call-site arguments lambda-list)
+      (return-from set-exclusive-or form))
+    (compute-compiler-macro-body
+     arguments
+     (butlast lambda-list)
+     '(set-exclusive-or-core
+       list-1 list-2
+       key key-supplied-p
+       test test-supplied-p
+       test-not test-not-supplied-p))))
