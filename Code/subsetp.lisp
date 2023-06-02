0c6af776393c5430401facfdd768ead55f819fd3
Robert Strandh <robert.strandh@gmail.com>
2023-06-02 12:57:29 +0200

    Define compiler macro for SUBSETP.

--- Code/subsetp.lisp
+++ Code/subsetp.lisp
@@ -29,3 +29,23 @@
                      test-not test-not-supplied-p))
 
 (declaim (notinline subsetp))
+
+(define-compiler-macro subsetp (&whole form &rest arguments)
+  (let ((lambda-list
+          '((list-1 list-2) ; required
+            ()           ; optional
+            nil          ; rest
+            ((:key key key-supplied-p)
+             (:test test test-supplied-p)
+             (:test-not test-not test-not-supplied-p))
+            nil)))
+    (unless (check-call-site arguments lambda-list)
+      (return-from subsetp form))
+    (compute-compiler-macro-body
+     arguments
+     (butlast lambda-list)
+     '(subsetp-core
+       list-1 list-2
+       key key-supplied-p
+       test test-supplied-p
+       test-not test-not-supplied-p))))
