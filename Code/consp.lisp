0dcd5c47d89c5bd14fd2c048b1336a1926eea030
Robert Strandh <robert.strandh@gmail.com>
2023-06-03 09:22:49 +0200

    Remove code for CONSP.  Must now be supplied by client.

--- Code/consp.lisp
+++ Code/consp.lisp
@@ -1,10 +1,7 @@
 (cl:in-package #:constrictor)
 
-(defgeneric consp (object)
-  (:method (object) nil)
-  (:method ((object cons)) t))
-
 (setf (documentation 'consp 'function)
       (format nil "Syntax: consp object~@
+                   ~@
                    Return T if OBJECT is a CONS cell.~@
                    Otherwise, return NIL."))
