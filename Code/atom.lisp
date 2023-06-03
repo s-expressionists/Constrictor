38d761607b061bc9c121f2ac9a8aac36ae87b5be
Robert Strandh <robert.strandh@gmail.com>
2023-06-03 09:24:00 +0200

    Remove code for ATOM.  Must now be supplied by client.

--- Code/atom.lisp
+++ Code/atom.lisp
@@ -1,11 +1,8 @@
 (cl:in-package #:constrictor)
 
-(defgeneric atom (object)
-  (:method (object) t)
-  (:method ((object cons)) nil))
-
 (setf (documentation 'atom 'function)
       (format nil "Syntax: atom object~@
+                   ~@
                    Return T if OBJECT is an atom, i.e.,~@
                    an object other than a CONS cell.~@
                    Return NIL if OBJECT is a CONS cell."))
