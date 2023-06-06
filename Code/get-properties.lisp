1b77a826d1dc4026fe8b6894e3b5d1ed5447c3c3
Robert Strandh <robert.strandh@gmail.com>
2023-06-06 08:48:00 +0200

    Provide documentation for GET-PROPERTIES.

--- Code/get-properties.lisp
+++ Code/get-properties.lisp
@@ -7,3 +7,19 @@
         when (member indicator indicator-list :test #'eq)
           return (values indicator value remaining)
         finally (return nil)))
+
+(setf (documentation 'get-properties 'function)
+      (format nil
+              "Syntax: get-properties plist indicator-list~@
+               ~@
+               This function searches the property list PLIST for~@
+               an indicator that is identical to one of the elements~@
+               of INDICATOR-LIST.  If there is such an indicator,~@
+               then this function returns three values which is the~@
+               first such indicator in PLIST, the first associated~@
+               value, and the remainder of PLIST starting at the~@
+               CONS cell immediately following the first value~@
+               ~@
+               If there is no indicator in PLIST that is identical~@
+               to one of the elements of INDICATOR-LIST, then this~@
+               function return the three values NIL, NIL, and NIL."))
