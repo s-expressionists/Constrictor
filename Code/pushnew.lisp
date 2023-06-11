64577197aae55081c63c814babb07fce73ecab72
Robert Strandh <robert.strandh@gmail.com>
2023-06-11 10:06:03 +0200

    Provide documentation for PUsHNEW.

--- Code/pushnew.lisp
+++ Code/pushnew.lisp
@@ -19,3 +19,20 @@
                                 ,test ,test-supplied-p
                                 ,test-not ,test-not-supplied-p)))
              ,writer-form)))))
+
+(setf (documentation 'pushnew 'function)
+      (format nil
+              "Syntax: pushnew item place &key key test test-not~@
+               ~@
+               This macro pushes ITEM to the front of the list that~@
+               is the value of PLACE, and updates PLACE accordingly,~@
+               if and only if ITEM is not already an element of that~@
+               list, as defined by KEY and TEST or TEST-NOT.~@
+               ~%~a~%~@
+               The function denoted by KEY is applied to each element~@
+               of the list that is the value of PLACE before the test~@
+               is applied.  KEY is typically used to extract a slot~@
+               from the element of the list to be used for the test,~@
+               but this is not a requirement.~@
+               ~%~a"
+              *key* *test-and-test-not*))
