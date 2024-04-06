584ceaa0c5f762a6ffc4f77bc2e425365fa89b93
Robert Strandh <robert.strandh@gmail.com>
2024-04-06 09:00:07 +0200

    Simplify implementation of NTH.

--- Code/nth.lisp
+++ Code/nth.lisp
@@ -3,20 +3,7 @@
 (declaim (inline nth))
 
 (defun nth (n list)
-  (unless (listp list)
-    (error 'list-expected :datum list))
-  (check-type n (integer 0))
-  (loop for result on list
-        repeat n
-        until (atom result)
-        count t into iteration-count
-        finally (cond ((and (= n iteration-count) (consp result))
-                       (return (car result)))
-                      ((null result)
-                       (return nil))
-                      (t (error 'dotted-list-with-too-few-cons-cells
-                                :minimum-cons-cell-count (1+ n)
-                                :offending-list list)))))
+  (car (nthcr n list)))
 
 (declaim (notinline nth))
 
