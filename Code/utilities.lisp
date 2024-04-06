d0e796cbf6f5439d2b3ba1b0ef6016714869e4ec
Robert Strandh <robert.strandh@gmail.com>
2024-04-06 08:42:54 +0200

    Simplify definition of PROPER-LIST-P.

--- Code/utilities.lisp
+++ Code/utilities.lisp
@@ -1,23 +1,7 @@
 (cl:in-package #:constrictor)
 
 (defun proper-list-p (list)
-  (cond ((null list)
-         0)
-        ((atom list)
-         (error 'list-must-be-proper-or-circular
-                :datum list))
-        (t
-         (let ((fast list)
-               (slow list)
-               (count 0))
-           (loop do (pop fast)
-                    (incf count)
-                 until (atom fast)
-                 do (pop fast)
-                    (incf count)
-                    (pop slow)
-                 until (or (atom fast) (eq fast slow))
-                 finally (return (null fast)))))))
+  (numberp (ignore-errors (list-length list))))
 
 (defun assert-proper-list (list)
   (unless (proper-list-p list)
