5b0c32d7fb61e3afe4d96227ab479dc45ad3f66f
Tarn W. Burton <twburton@gmail.com>
2024-06-04 10:54:29 -0400

    Use ADJOIN vs ADJOIN-CORE to preserve eval order

--- Code/pushnew.lisp
+++ Code/pushnew.lisp
@@ -3,10 +3,9 @@
 (defmacro pushnew
     (item place
      &environment environment
-     &key
-       (key nil key-supplied-p)
-       (test nil test-supplied-p)
-       (test-not nil test-not-supplied-p))
+     &rest rest
+     &key key (test nil test-supplied-p) (test-not nil test-not-supplied-p))
+  (declare (ignore key test test-not))
   (if (and test-supplied-p test-not-supplied-p)
       (progn (warn 'warn-both-test-and-test-not-given)
              `(error 'both-test-and-test-not-given))
@@ -14,10 +13,7 @@
           (get-setf-expansion place environment)
         `(let (,@(mapcar #'list vars vals))
            (let ((,(car store-vars)
-                   (adjoin-core ,item ,reader-form
-                                ,key ,key-supplied-p
-                                ,test ,test-supplied-p
-                                ,test-not ,test-not-supplied-p)))
+                   (adjoin ,item ,reader-form ,@rest)))
              ,writer-form)))))
 
 (setf (documentation 'pushnew 'function)
