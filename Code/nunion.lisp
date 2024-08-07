5a3c1ac7ab4aeb7f362b46b4d104d7dd72d0cdd3
Robert Strandh <robert.strandh@gmail.com>
2024-08-07 16:27:54 +0200

    Provide documentation.

--- Code/nunion.lisp
+++ Code/nunion.lisp
@@ -53,3 +53,39 @@
        key key-supplied-p
        test test-supplied-p
        test-not test-not-supplied-p))))
+
+(setf (documentation 'nunion 'function)
+      (format nil
+              "Syntax: nunion list-1 list-2 &key key test test-not~@
+               ~%~a~%~%~a~%~%~a~%~%~a~%~@
+               Either TEST or TEST-NOT is applied to the result of~@
+               applying KEY to an element of LIST-1 and and the result~@
+               of applying KEY to an element of LIST-2 in that order.~@
+               For elements E1 of LIST-1 and E2 of LIST-2, if the test~@
+               is satisfied, either E1 or E2 (but not both) will be~@
+               included in the return value of this function.  Any~@
+               element E1 of LIST-1 for which there is no element E2~@
+               in LIST-2 that satisfies the test will be included in~@
+               in the return value of this function.  Similarly, any~@
+               element E2 of LIST-2 for which there is no element E1~@
+               in LIST-1 that satisfies the test will be included in~@
+               in the return value of this function.
+               ~@
+               This function may modify LIST-1 in that new elements may~@
+               be added to it.  The original value of LIST-1 should not~@
+               be used after a call to this function.
+               ~@
+               The order of the elements in the return value of~@
+               this function should not be relied upon.  If there are~@
+               duplicate elements in LIST-1, and there is no corresponding~@
+               element in LIST-2 that satifies the test, then the return~@
+               value of this function will also contain duplicate elements.~@
+               Similarly, if there are duplicate elements in LIST-1, and~@
+               there is no corresponding element in LIST-2 that satifies~@
+               the test, then the return value of this function will also~@
+               contain duplicate elements."
+              *list-1-and-list-2-must-be-proper*
+              *key*
+              *key-applied-to-elements-of-list-1-and-list-1*
+              *test-and-test-not*))
+                     
