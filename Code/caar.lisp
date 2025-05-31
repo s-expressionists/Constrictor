f495d9208914d16c847c64cdccd65e87defb497c
Robert Strandh <robert.strandh@gmail.com>
2025-05-31 13:05:02 +0200

    Provide documentation string for CAAR.

--- Code/caar.lisp
+++ Code/caar.lisp
@@ -7,3 +7,10 @@
   (car (car list)))
 
 (declaim (notinline caar))
+
+(setf (documentation 'caar 'function)
+      (format nil
+              "Syntax: caar list~@
+               ~@
+               A call to this function is equivalent in all possible~@
+               respects to a call (car (car list))"))
